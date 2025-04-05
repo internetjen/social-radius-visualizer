19bb949163e6473cb2a0bb8fb1a336dda2e8bfc1
Jennifer Arboleda <arboleda.jen@gmail.com>
2025-04-05 18:14:52 -0500

    creates marker at searched address, adds to map, and zoom animation to new location

--- leaflet.js
+++ leaflet.js
@@ -1,5 +1,5 @@
 // This is a simple Leaflet map example
-var map = L.map('map').setView([39.8283, -98.5795], 5); // Set initial coordinates and zoom level
+var map = L.map('map').setView([39.8283, -98.5795], 4); // Set initial zoom and coordinates
 
 // Add a tile layer (credit: OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,7 +7,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //This script allows a user to type an address into a search box, click search, and retrieve the latitude and longitude of that address using the Nominatim API
-
 //Address search function
 document.getElementById('searchButton').addEventListener('click', function() { // Get the address from the input field when Search button is clicked
     var address = document.getElementById('addressInput').value; 
@@ -22,10 +21,23 @@ function geocodeAddress(address) {
     .then(data => {
         console.log(data); //Logs the data to the console so you can inspect what was returned.
 
-        if (data.length > 0) { //Pulls the latitude and longitude from the first result (data[0]).
-        const lat = data[0].lat;
-        const lon = data[0].lon;
+        if (data.length > 0) { //Checks if any results were returned
+        const lat = parseFloat(data[0].lat); // Pulls the Lat/Lon from the first result (data[0]) and converts Lat string to number
+        const lon = parseFloat(data[0].lon); // Pulls the Lat/Lon from the first result (data[0]) and converts Lon string to number
         console.log(`Latitude: ${lat}, Longitude: ${lon}`); //Logs the coordinates to the console
+
+        //Fly to and zoom into searched location
+        map.flyTo([lat, lon], 12, {
+            duration: 2 //Duration of effect
+        });
+
+        //Add marker of searched location
+        setTimeout(() => {
+        L.marker([lat, lon]) //Creates marker at searched address, adds to map, and pops up
+            .addTo(map)
+            .bindPopup(`<b>${address}</>`);
+        }, 2000); // Matches duration of flyTo effect
+
     } else {
         alert("Address not found."); //If no results were returned, it alerts the user that the address wasn't found.
     }
